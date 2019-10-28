833c2772a8537106a0fb0d351acf9fd6fc5ed4f7
Koscial <jan.koscialkowski@gmail.com>
2019-10-28 23:25:15 +0100

    Finished the app, an API for Pokemon info ready

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 #!/home/ubuntu/simple-web-app/venv/bin/python3
+import pandas as pd
+
 from flask import Flask
 
 app = Flask(__name__)
 # CORS(app) #Prevents CORS errors
 
 
+# Read the Pokemon dataset
+pokemon_df = pd.read_csv('pokemon.csv')
+
+
 @app.route('/app')
 def index():
-    return 'The Flask app works!'
+    html = """
+    <h2>The Flask app works!</h2>
+    Here is the Pokemon <a href='/app/pokemon'>app</a>.
+    """
+    return html
+
+
+@app.route('/app/pokemon')
+def pokemon_empty():
+    return '<h2>Type the Pokemon name after the slash in the address bar.</h2>'
+
+
+@app.route('/app/pokemon/<pokemon_name>', methods=['GET'])
+def get_pokemon(pokemon_name):
+    logical = pokemon_df.name.apply(lambda x: x.lower()) == pokemon_name.lower()
+    return pokemon_df[logical].to_json()
 
 
 if __name__ == '__main__':
