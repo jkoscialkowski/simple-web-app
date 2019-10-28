3abee0fb47b2eab37ef91dcdd11250ef3d2d3b45
Koscial <jan.koscialkowski@gmail.com>
2019-10-29 00:56:03 +0100

    Bugfixes

--- init.sh
+++ init.sh
@@ -10,16 +10,12 @@ python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-# Start gunicorn
-gunicorn --workers 3 --bind unix:app.sock -m 007 wsgi:app
-
-# Run the app
-chmod a+x app.py
-nohup ./app.py &
-
 # Install NGINX
 sudo apt-get install nginx
 
 # Copy the NGINX conf file and restart NGINX
 sudo cp nginx.conf /etc/nginx/
 sudo service nginx restart
+
+# Start gunicorn
+gunicorn --workers 3 --bind 0.0.0.0:5000 wsgi:app --daemon
